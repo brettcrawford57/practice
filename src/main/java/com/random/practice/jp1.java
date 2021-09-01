851f0095dd39e0441a28c36d62b6ccaf4997a49f
Brett Crawford <brett.crawford57@gmail.com>
2021-09-01 17:44:29 -0500

    jp2 practice

---
+++ src/main/java/com/random/practice/jp1.java
@@ -0,0 +1,7 @@
+package com.random.practice;
+
+public class jp1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/main/java/com/random/practice/jp2.java
@@ -0,0 +1,8 @@
+package com.random.practice;
+
+public class jp2 {
+    public static void main(String[] args){
+        double pi = 3.14159;
+        System.out.print("The value of pi is approximately " + pi);
+    }
+}
